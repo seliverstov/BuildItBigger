c17bb2563fd9954645060b5a6551d4ced8bd3a09
Alexander Seliverstov <seliverstov.a@gmail.com>
2016-01-12 17:07:57 +0300

    Add activity to display jokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.seliverstov.jokes.Jokes;
+import com.seliverstov.jokesactivity.JokesActivity;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -42,7 +44,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, Jokes.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, Jokes.getRandomJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, JokesActivity.class);
+        intent.putExtra(Intent.EXTRA_TEXT,Jokes.getRandomJoke());
+        startActivity(intent);
     }
 
 
---
+++ jokesactivity/src/main/java/com/seliverstov/jokesactivity/JokesActivity.java
@@ -0,0 +1,36 @@
+package com.seliverstov.jokesactivity;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar!=null)
+            actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+        TextView joke = (TextView)findViewById(R.id.joke);
+        joke.setText(text);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home: finish(); return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+
+    }
+}
